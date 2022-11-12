a3b5d98ac00327a2bec0bb930093581aa749c47c
muhammadanjothosatru <muhammadanjothosatru@gmail.com>
2022-11-12 14:18:50 +0700

    home

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -9,45 +9,45 @@ import {
   useColorModeValue,
   Stack,
   HStack,
-} from '@chakra-ui/react'
-import { SunIcon, MoonIcon } from '@chakra-ui/icons'
-import { NavLink } from 'react-bootstrap';
-import Link from 'next/link'
+} from "@chakra-ui/react";
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
+import Link from "next/link";
 
 function NavbarTest() {
-  const {colorMode, toggleColorMode} = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
+  
   return (
     <>
-      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-        <HStack spacing={8} alignItems={'center'}>
-            <Box>Logo</Box>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"} px={20}>
+          <HStack spacing={8} alignItems={"center"}>
+            <Menu>
+              <MenuButton rounded={"full"} minW={0}>
+                <Avatar
+                  size={"sm"}
+                  src={"https://avatars.dicebear.com/api/male/username.svg"}
+                />
+              </MenuButton>
+            </Menu>
             <HStack
-              as={'nav'}
+              as={"nav"}
               spacing={4}
-              display={{ base: 'none', md: 'flex' }}>
-              <Link href='/'>Home</Link>
-              <Link href='/portofolio'>Portofolio</Link>
+              display={{ base: "none", md: "flex" }}
+            >
+              <Link href="/">Home</Link>
+              <Link href="/portofolio">Portofolio</Link>
             </HStack>
           </HStack>
-          
 
-          <Flex alignItems={'center'}>
-            <Stack direction={'row'} spacing={7}>
+          <Flex alignItems={"center"}>
+            <Stack direction={"row"} spacing={7}>
               <Button onClick={toggleColorMode}>
-                {colorMode === 'light' ? <MoonIcon /> : <SunIcon color="yellow.400"/>}
+                {colorMode === "light" ? (
+                  <MoonIcon />
+                ) : (
+                  <SunIcon color="yellow.400" />
+                )}
               </Button>
-
-              <Menu>
-                <MenuButton
-                  rounded={'full'}
-                  minW={0}>
-                  <Avatar
-                    size={'sm'}
-                    src={'https://avatars.dicebear.com/api/male/username.svg'}
-                  />
-                </MenuButton>
-              </Menu>
             </Stack>
           </Flex>
         </Flex>
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,48 @@
 import {
-  Container,
+  Container, useColorMode, Stack, Flex, Heading, Text, Button
 } from '@chakra-ui/react'
 import Head from 'next/head';
+import saveAs from 'file-saver'
 
 export default function Home() {
-
+  const { colorMode } = useColorMode()
+  const colorSecondary = {
+    light: 'gray.700',
+    dark: 'gray.400'
+  }
+  const saveFile = () => {
+    saveAs(
+      "https://drive.google.com/file/d/1TihnMT0tmJcVRd20Klfn-o8Khfb3Sagv/view?usp=share_link"
+    );
+  }
   return (
     <Container>
       <Head>
         <title>Home - Muhammad Anjotho Satru</title>
       </Head>
+      <Stack
+        as="main"
+        spacing={8}
+        justifyContent="center"
+        alignItems="flex-start"
+        m="0 auto 4rem auto"
+        maxWidth="700px"
+        px={2}
+      >
+        <Flex
+          flexDirection="column"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          maxWidth="700px"
+          py={20}
+        >
+          
+          <Heading mb={2}>Hi, I'm Muhammad Anjotho Satru</Heading>
+          <Text color={colorSecondary[colorMode]}>Graduate from Computer Engineering at Institut Teknologi Sepuluh Nopember. I have passion in technology. Especially in Web Development, Cloud Computing, and Network</Text>
+          <br></br>
+          <Button onClick={saveFile}>View Resume</Button>
+        </Flex>
+      </Stack>
     </Container>
     
   )
