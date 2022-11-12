33eb0412ad0b554fafc1d52f0b2516863745c827
muhammadanjothosatru <muhammadanjothosatru@gmail.com>
2022-11-12 10:57:33 +0700

    first commit

---
+++ pages/components/navbar.js
@@ -0,0 +1,71 @@
+import {
+  Container,
+  Text,
+  useColorMode,
+  IconButton,
+  Box,
+  Flex,
+  Avatar,
+  Link,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  HStack,
+  Center,
+} from '@chakra-ui/react'
+import { SunIcon, MoonIcon } from '@chakra-ui/icons'
+import Head from 'next/head';
+import { NavLink } from 'react-bootstrap';
+
+function NavbarTest() {
+  const {colorMode, toggleColorMode} = useColorMode();
+  return (
+    <>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+        <HStack spacing={8} alignItems={'center'}>
+            <Box>Logo</Box>
+            <HStack
+              as={'nav'}
+              spacing={4}
+              display={{ base: 'none', md: 'flex' }}>
+              <NavLink>Home</NavLink>
+              <NavLink>Portofolio</NavLink>
+            </HStack>
+          </HStack>
+          
+
+          <Flex alignItems={'center'}>
+            <Stack direction={'row'} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              </Button>
+
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={'full'}
+                  variant={'link'}
+                  cursor={'pointer'}
+                  minW={0}>
+                  <Avatar
+                    size={'sm'}
+                    src={'https://avatars.dicebear.com/api/male/username.svg'}
+                  />
+                </MenuButton>
+              </Menu>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
+
+export default NavbarTest;
